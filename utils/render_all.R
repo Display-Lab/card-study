7fc95173ef625cf6bd97622e833f0167ef9377dd
Colin Gross <grosscol@gmail.com>
2020-05-28 11:54:46 -0400

    Replace render all cards utility.

---
+++ utils/render_all.R
@@ -0,0 +1,11 @@
+# Render all cards
+library(rmarkdown)
+
+from_base_files <- list.files(path="cards", pattern = "\\.Rmd$", full.names = T)
+from_cards_files <- list.files(path=".", pattern = "\\.Rmd$")
+files <- c(from_base_files, from_cards_files)
+
+for (f in files){
+  render(f, output_format="pdf_document", output_dir="build/")
+}
+
